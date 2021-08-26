7bc2db95bf72c900aed3c69ccd956daba1430025
Maria Lomeli <lomeli.aiengineer@gmail.com>
2021-08-26 12:33:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Enviar correos masivos desde Google Sheets
+
+## Funciones
+
+* SendEmail.js
+
+Esta función recorreo las filas de hoja de cálculo y le envía a cada uno contacto un correo electrónico con un asusto y un mensaje.
+
+* SendEmailWithImg.js
+
+Esta función recorre las filas de una hoja de cálculo y le envía a cada contacto un correo electrónico con un asusto, un mensaje y una imagen como adjunto.
+
+## Stack Tecnológico
+
+* Apps Script
+* Google Sheets
+
+
---
+++ sendEmail.js
@@ -0,0 +1,17 @@
+function sendEmail() {
+    var ss = SpreadsheetApp.getActiveSpreadsheet()
+    var sheet1 = ss.getSheetByName('Contactos');
+    var sheet2 = ss.getSheetByName('Mensaje');
+    var subject = sheet2.getRange(2, 1).getValue();
+    var message = sheet2.getRange(2, 2).getValue();
+    var n = sheet1.getLastRow();
+    
+    for (var i = 2; i < n + 1; i++) {
+        var emailAddress = sheet1.getRange(i, 3).getValue();
+        var name = sheet1.getRange(i, 1).getValue();
+        var lastname = sheet1.getRange(i, 2).getValue();
+
+        message = message.replace("<nombre>", name).replace("<apellido>", lastname);
+        MailApp.sendEmail(emailAddress, subject, message);
+    }
+}
